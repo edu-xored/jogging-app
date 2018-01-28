c3eed3e5ae20e02566fed5a763a8e02be2e54897
bondiano <batbondik0@gmail.com>
2018-01-28 21:22:52 +0700

    Add material design, firebase dependence

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
-
 
 @NgModule({
   declarations: [
@@ -12,7 +12,8 @@ import { AppRoutingModule } from './/app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

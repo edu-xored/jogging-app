4d30e4e5f9c4cc681242b529b5b2b23a06f49c2c
AlOb <obukhova.alisa@gmail.com>
2018-01-30 07:31:14 +0700

    rubbish removed, formatting

--- db/db.ts
+++ db/db.ts
@@ -1,6 +1,6 @@
-import {database} from "firebase";
-import {firebaseConfig} from "../src/environments/firebase.config";
-import {dbData} from "./db-data";
+import { database } from 'firebase';
+import { firebaseConfig } from '../src/environments/firebase.config';
+import { dbData } from '/db-data';
 
-console.log("Initializing firebase database...");
+console.log('Initializing firebase database...');
 

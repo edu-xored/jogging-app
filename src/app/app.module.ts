74545b7b1ed807de523a02f16b4cb204bf5ef047
AlOb <obukhova.alisa@gmail.com>
2018-01-30 07:24:20 +0700

    added: header component, db filesm edited firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,33 @@
+/** Common modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routes';
+import { firebaseConfig } from '../environments/firebase.config';
+
+/** Firebase modules */
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+/** Material design modules */
+import { MatButtonModule } from '@angular/material/button';
+
+/** Components */
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
     AppRoutes,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

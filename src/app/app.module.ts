372c40894f8395f14a488d4c4eee8423596d7a56
asgeneral <asgeneral@yandex.ru>
2018-02-02 14:30:42 +0700

    Created header without connection to server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
@@ -34,6 +34,7 @@ import { HeaderComponent } from './header/header.component';
     AngularFirestoreModule,
     routes,
     BrowserAnimationsModule,
+    MatButtonModule,
     MatToolbarModule,
   ],
   providers: [UserService, RecordService],

50e9a33a8a43dbe3458d6d635e6d2971e2a800e8
asgeneral <asgeneral@yandex.ru>
2018-02-02 17:45:13 +0700

    Header completed.(#7)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user/user.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HeaderComponent {
+  constructor(private userService: UserService) { }
+  isAuthorised = this.userService.isUserAuthorized();
 }
+
+

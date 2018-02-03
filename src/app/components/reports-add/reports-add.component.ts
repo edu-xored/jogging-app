e7a4d9ddd1e057aff6550258d49da2d0ef3a0224
AlOb <obukhova.alisa@gmail.com>
2018-02-03 22:38:41 +0700

    Merge pull request.

--- src/app/components/reports-add/reports-add.component.ts
+++ src/app/components/reports-add/reports-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportService } from '../../services/report/report.service';
+import { Report } from '../../models/Report';
 
 @Component({
   selector: 'app-reports-add',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports-add.component.css']
 })
 export class ReportsAddComponent implements OnInit {
+
+  reportDetails = {
+    distance: 0,
+    time: ''
+  };
+
   startDate = new Date();
-  constructor() { }
+  constructor(public reportService: ReportService) { }
 
   ngOnInit() {
+  }
+
+  addReport() {
+    // this.reportService.addReport(new Report())
   }
 
 }
--- src/app/components/reports/reports.component.ts
+++ src/app/components/reports/reports.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource, MatSort } from '@angular/material';
+import { ReportService } from '../../services/report/report.service';
 
 @Component({
   selector: 'app-reports',
@@ -7,8 +8,10 @@ import { MatTableDataSource, MatSort } from '@angular/material';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent {
-  displayedColumns = ['Date', 'Distance', 'Time'];
+  displayedColumns = ['Date', 'Distance', 'Time', 'Delete'];
   dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  constructor(public reportService: ReportService) { }
 
   @ViewChild(MatSort) sort: MatSort;
 
@@ -20,18 +23,24 @@ export class ReportsComponent {
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
+
+  deleteReport(id: number) {
+    console.log(id);
+    // this.reportService.deleteReport(id);
+  }
 }
 
 export interface Element {
+  id: number;
   date: string;
   distance: number;
   time: string;
 }
 
 const ELEMENT_DATA: Element[] = [
-  {date: '11.03.2018', distance: 5000, time: '00:12:30'},
-  {date: '11.04.2018', distance: 1000, time: '00:13:30'},
-  {date: '11.05.2018', distance: 2500, time: '00:14:31'},
-  {date: '11.06.2018', distance: 3000, time: '00:15:30'},
-  {date: '11.08.2018', distance: 1200, time: '00:20:30'},
+  {id: 0, date: '11.03.2018', distance: 5000, time: '00:12:30'},
+  {id: 1, date: '11.04.2018', distance: 1000, time: '00:13:30'},
+  {id: 2, date: '11.05.2018', distance: 2500, time: '00:14:31'},
+  {id: 3, date: '11.06.2018', distance: 3000, time: '00:15:30'},
+  {id: 4, date: '11.08.2018', distance: 1200, time: '00:20:30'},
 ];

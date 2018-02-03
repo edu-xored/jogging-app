8433ba33d2ad06239f3ed7d39a17efff595cb554
bondiano <batbondik0@gmail.com>
2018-02-03 14:08:55 +0700

    Add reports cpmpoments to router

--- src/app/core/app.routes.ts
+++ src/app/core/app.routes.ts
@@ -3,11 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { LoginComponent } from '../components/login/login.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
+import { ReportsComponent } from '../components/reports/reports.component';
+import { ReportsEditComponent } from '../components/reports-edit/reports-edit.component';
+
 import { AuthGuard } from './auth.guard';
 
 const router: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '',   redirectTo: '/login', pathMatch: 'full' },
+  { path: 'reports', component: ReportsComponent, canActivate: [AuthGuard] },
+  { path: 'reports-edit', component: ReportsEditComponent, canActivate: [AuthGuard] },
+  { path: '',   redirectTo: '/reports', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
   // TODO: for '/records' path use `canActivate: [AuthGuard]`
 ];
